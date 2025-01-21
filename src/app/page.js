2dbc7602ebac42b46c5e2e44828547b58e0c7b15
Gaivya <gaivyasen@gmail.com>
2025-01-21 16:27:47 +0530

    first change

---
+++ src/app/page.js
@@ -0,0 +1,23 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <div>
+        <button
+          aria-label="Increment value"
+          onClick={() => dispatch(increment())}
+        >
+          Increment
+        </button>
+        <span>{count}</span>
+        <button
+          aria-label="Decrement value"
+          onClick={() => dispatch(decrement())}
+        >
+          Decrement
+        </button>
+      </div>
+    </div>
+  );
+}
